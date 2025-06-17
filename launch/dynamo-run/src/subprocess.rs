ac4fd87b749a4cb05ceea3804ccd1c35f08e0c4f
Ryan McCormick <rmccormick@nvidia.com>
2025-06-18 06:35:14 +0800

    refactor: Log subprocess stderr as WARN (#1563)

--- launch/dynamo-run/src/subprocess.rs
+++ launch/dynamo-run/src/subprocess.rs
@@ -98,7 +98,11 @@ pub async fn start(
     tokio::spawn(async move {
         let mut lines = stderr.lines();
         while let Ok(Some(line)) = lines.next_line().await {
-            tracing::error!("{}", strip_log_prefix(&line));
+            // FIXME: always logging INFO/DEBUG will hide real errors, but
+            // some libraries log non-errors to stderr, which confuses users
+            // when we log those as ERROR. Using WARN as a middle ground for
+            // now, but we can probably be smarter here.
+            tracing::warn!("{}", strip_log_prefix(&line));
         }
     });
 
