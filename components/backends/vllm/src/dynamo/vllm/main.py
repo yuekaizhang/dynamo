e2a514b2ad293684889f5615913b7080aea5d333
Alec <35311602+alec-flowers@users.noreply.github.com>
2025-07-27 22:42:45 -0700

    fix: remove prints (#2142)

--- components/backends/vllm/src/dynamo/vllm/main.py
+++ components/backends/vllm/src/dynamo/vllm/main.py
@@ -185,8 +185,6 @@ async def init(runtime: DistributedRuntime, config: Config):
 
         handler.kv_publisher = kv_publisher
 
-    print(f"FINAL: {engine_client.vllm_config.cache_config.enable_prefix_caching}")
-    print(f"FINAL: {engine_client.vllm_config.kv_events_config}")
     try:
         await asyncio.gather(
             generate_endpoint.serve_endpoint(handler.generate),
