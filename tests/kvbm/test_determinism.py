88d953bfb8b03facd6cee46b55a6d405715fdbbb
Olga Andreeva <124622579+oandreeva-nv@users.noreply.github.com>
2025-08-21 12:41:56 -0700

    test: Temporary exclude kvbm tests from vllm, nightly, gpu1... (#2611)

--- tests/kvbm/test_determinism.py
+++ tests/kvbm/test_determinism.py
@@ -28,12 +28,13 @@ import pytest
 import requests
 
 # Test markers to align with repository conventions
+# Todo: enable the rest when kvbm is built in the ci
 pytestmark = [
     pytest.mark.kvbm,
-    pytest.mark.e2e,
-    pytest.mark.slow,
-    pytest.mark.nightly,
-    pytest.mark.gpu_1,
+    #    pytest.mark.e2e,
+    #    pytest.mark.slow,
+    #    pytest.mark.nightly,
+    #    pytest.mark.gpu_1,
 ]
 
 
